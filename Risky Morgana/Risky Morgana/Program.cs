79af7ef9eea41ae262f9fc422a6dad0c43cec852
Risky <rizkyteuw@gmail.com>
2015-01-01 21:31:52 +0200

    Started :3
    Risky Morgana REWORKED.

---
+++ Risky Morgana/Risky Morgana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risky_Morgana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
